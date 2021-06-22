1ea6bb92312d45f04e1967bc5ae3e2e9f2daebd8
petercolella <peter.joseph.colella@gmail.com>
2021-06-22 14:38:22 -0400

    deletes unnecessary files

--- src/utils/viz.js
+++
@@ -1,8 +0,0 @@
-import * as vl from "vega-lite-api";
-export const viz = vl
-  .markLine({ size: 5, opacity: 1 })
-  .encode(
-    vl.x().fieldT("timestamp").scale({ zero: false }),
-    vl.y().fieldQ("temperature").scale({ zero: false }),
-    vl.tooltip().fieldN("temperature")
-  );
