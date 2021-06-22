0c38482d96346c74e5aca97d77c10cacdb45d3c0
petercolella <peter.joseph.colella@gmail.com>
2021-06-22 13:48:55 -0400

    updates FetchContainer with pie chart logic

--- src/FetchContainer.js
+++ src/FetchContainer.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { csv } from "d3";
+import { arc, csv } from "d3";
+import useWindowDimensions from "./hooks/useWindowDimensions";
 
 const csvUrl =
   "https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/acd2b8cecfe51c520622fbaf407ee88b8796bfc6/cssNamedColors.csv";
@@ -12,6 +13,21 @@ const styles = {
 
 const FetchContainer = () => {
   const [data, setData] = useState(null);
+  const { height, width } = useWindowDimensions();
+
+  const centerX = width / 2;
+  const centerY = height / 2;
+
+  const arcInstance = arc();
+  const piePieceDegree = data && (Math.PI * 2) / data.length;
+
+  const pieArc = (i) =>
+    arcInstance({
+      innerRadius: 0,
+      outerRadius: height < width ? height / 2 : width / 2,
+      startAngle: piePieceDegree * i,
+      endAngle: piePieceDegree * (i + 1),
+    });
 
   useEffect(() => {
     csv(csvUrl).then(setData);
@@ -21,14 +37,15 @@ const FetchContainer = () => {
     return <pre style={styles.pre}>Waiting for data...</pre>;
   }
 
-  console.log(data[0]);
-
-  return data.map((d, i) => (
-    <div
-      key={i}
-      style={{ backgroundColor: d["RGB hex value"], height: 20 }}
-    ></div>
-  ));
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        {data.map((d, i) => (
+          <path key={i} fill={d["RGB hex value"]} d={pieArc(i)} />
+        ))}
+      </g>
+    </svg>
+  );
 };
 
 export default FetchContainer;
